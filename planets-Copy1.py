4dd125b390f5efb00799f07160937816769f48c0
chrismiklasevich <39773626+chrismiklasevich@users.noreply.github.com>
2018-09-23 22:25:23 -0700

    Add files via upload

---
+++ planets-Copy1.py
@@ -0,0 +1,22 @@
+#Name: Christopher Miklasevich
+#Student ID:
+#Date (last modified):9/23
+#
+#Lab 0
+#Section:15
+#Purpose of Lab: To figure out how to use github
+#Additional Comment:
+#Function to calculate and print out the weight on Mars and Jupiter
+def weight_on_planets():
+    w = int( input( "What do you weigh on earth? "))
+    print( "\n On Mars you would weigh " ,w * 0.38, " pounds \n On Jupiter you would weigh " ,w*2.34, "pounds" )
+    
+        
+   
+   
+   
+
+
+
+if __name__ == '__main__':
+   weight_on_planets()
---
+++ planets_tests-Copy1.py
@@ -0,0 +1,30 @@
+import unittest
+import io, sys
+from planets import *
+#Created a class for the test cases
+class Test_planets(unittest.TestCase):
+
+    def test_01(self):
+        sys.stdin = io.StringIO("136")
+        sys.stdout = student_output = io.StringIO()        
+        expected_out = "What do you weigh on earth? \nOn Mars you would weigh 51.68 pounds.\nOn Jupiter you would weigh 318.24 pounds."
+        weight_on_planets()
+        #print(student_output.getvalue().strip())
+        self.assertEqual(expected_out, student_output.getvalue().strip())
+    def test_02(self):
+        sys.stdin = io.StringIO("1")
+        sys.stdout = student_output = io.StringIO()        
+        expected_out = "What do you weigh on earth? \nOn Mars you would weigh 0.38 pounds.\nOn Jupiter you would weigh 2.34 pounds."
+        weight_on_planets()
+        #print(student_output.getvalue().strip())
+        self.assertEqual(expected_out, student_output.getvalue().strip())
+    def test_03(self):
+        sys.stdin = io.StringIO("100")
+        sys.stdout = student_output = io.StringIO()        
+        expected_out = "What do you weigh on earth? \nOn Mars you would weigh 38 pounds.\nOn Jupiter you would weigh 234 pounds."
+        weight_on_planets()
+        #print(student_output.getvalue().strip())
+        self.assertEqual(expected_out, student_output.getvalue().strip())
+
+if __name__ == "__main__":
+    unittest.main()
