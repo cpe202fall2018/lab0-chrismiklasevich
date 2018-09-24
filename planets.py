c90252785e650391da09f4dd2ae4aecdf9ba48ae
chrismiklasevich <39773626+chrismiklasevich@users.noreply.github.com>
2018-09-24 13:13:39 -0700

    Add files via upload

--- planets.py
+++ planets.py
@@ -1,7 +1,22 @@
+#Name: Christopher Miklasevich
+#Student ID: 169844
+#Date (last modified): 9/24
+#
+#Lab 0
+#Section: 15
+#Purpose of Lab: To understand how to use github
+#Additional Comment:
+# This is to figure out the weight on Mars and Jupiter
 def weight_on_planets():
-   # write your code here
+    w = float( input( "What do you weigh on earth? "))
+    print( "\nOn Mars you would weigh" ,w * 0.38, "pounds.\nOn Jupiter you would weigh" ,w*2.34,"pounds." )
+    
+        
    
    
    
+
+
+
 if __name__ == '__main__':
    weight_on_planets()
--- planets_tests.py
+++ planets_tests.py
@@ -11,7 +11,20 @@ class Test_planets(unittest.TestCase):
         weight_on_planets()
         #print(student_output.getvalue().strip())
         self.assertEqual(expected_out, student_output.getvalue().strip())
-
+    def test_02(self):
+        sys.stdin = io.StringIO("1")
+        sys.stdout = student_output = io.StringIO()        
+        expected_out = "What do you weigh on earth? \nOn Mars you would weigh 0.38 pounds.\nOn Jupiter you would weigh 2.34 pounds."
+        weight_on_planets()
+        #print(student_output.getvalue().strip())
+        self.assertEqual(expected_out, student_output.getvalue().strip())
+    def test_03(self):
+        sys.stdin = io.StringIO("100")
+        sys.stdout = student_output = io.StringIO()        
+        expected_out = "What do you weigh on earth? \nOn Mars you would weigh 38.0 pounds.\nOn Jupiter you would weigh 234.0 pounds."
+        weight_on_planets()
+        #print(student_output.getvalue().strip())
+        self.assertEqual(expected_out, student_output.getvalue().strip())
 
 if __name__ == "__main__":
-        unittest.main()
+   unittest.main()
